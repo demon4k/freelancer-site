5481194f58907a213a6f29c1fe532f0c9d9b4994
Dmytro Parhomchuk <Dmytroparhomchuk90@gmail.com>
2025-06-12 00:51:44 +0300

    Практична 3: додано моделі Category і Product, додано 10 товарів через адмінку

---
+++ freelancer/main/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    stock = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=True)
+    image_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
